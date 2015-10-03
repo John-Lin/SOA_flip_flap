3843bcc6728a89d779642de2dcad8ddd80247d03
John-Lin <linton.tw@gmail.com>
2015-10-04 01:08:58 +0800

    Added code

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,12 +7,34 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    survey, lines = [], []
+    tsv.each_line { |line| lines << line }
+    keys = lines[0].split("\t")
+    keys.map!(&:chomp)
+    lines.shift
+    lines.each do |line|
+      values = line.split("\t")
+      record = Hash.new
+      keys.each_index { |index| record[keys[index]] = values[index].chomp }
+      survey.push(record)
+    end
+    @data = survey
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    line = ''
+    keys_array = @data[0].keys
+    line << keys_array[0]
+    keys_array.shift
+    keys_array.each { |key| line << "\t" + key }
+    line << "\n"
+    @data.each do |record|
+      line << record.map{|k,v| "#{v}"}.join("\t")
+      line << "\n"
+    end
+    line.chomp("\t")
+    line
   end
 end
